a45ed0f3995b583709c9fc1474d0ae64482e9925
Jarda Korinek <korinek14.j@gmail.com>
2022-11-04 23:35:45 +0100

    typescript + eslint + prettier

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,5 @@
-import React from "react"
+import React from 'react';
 
 export default function Home() {
-  interface Name {
-    name: string
-  }
-
-  const karel: Name = 5
-
-  console.log(karel)
-
-  return <div className="karel">Hello world!</div>
+  return <div>Hello world!</div>;
 }
