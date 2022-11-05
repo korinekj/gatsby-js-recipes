2f06a9e2632bd4b0a5386b93839c7c7ab497d0fc
Jarda Korinek <korinek14.j@gmail.com>
2022-11-05 23:09:16 +0100

    nested files structure

---
+++ src/pages/company/history.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function History() {
+  return <div>History of our company</div>;
+}
+
+export default History;
---
+++ src/pages/company/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function CompanyIndex() {
+  return <div>index file of company</div>;
+}
+
+export default CompanyIndex;
