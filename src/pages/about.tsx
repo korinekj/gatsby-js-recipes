4d1c140b12e71fca2a554221399b52a07f2f68c5
Jarda Korinek <korinek14.j@gmail.com>
2022-11-07 23:00:12 +0100

    CSS modules -> added declarations.d.ts file to src folder for module imports to work; added include gatsby-config.js to tsconfig.eslint.json for removing eslint error in gatsby-config.js

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 import Layout from '../components/Layout';
 
+import * as styles from '../examples/about.module.scss';
+
 function About() {
   return (
     <Layout>
+      <div className={styles.page}>
+        <h1>About page</h1>
+        <h1>Hello world</h1>
+        <p className={styles.text}>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo cumque
+          consectetur assumenda labore provident sapiente incidunt, nobis ab
+          nesciunt voluptas quasi laboriosam fugiat at laborum id odit
+          distinctio quidem. Porro.
+        </p>
+      </div>
       <h2>Hello from about page</h2>
     </Layout>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,22 +2,22 @@ import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/Layout';
 
+import { page, text } from '../examples/home.module.scss';
+
 export default function Home() {
   return (
     <Layout>
-      <h1>Home page</h1>
-
-      <div>
-        <Link to='/about'>About</Link>
+      <div className={page}>
+        <h1>Home page</h1>
+        <h1>hello world</h1>
+        <h1>hello people</h1>
+        <p className={text}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis
+          soluta cum exercitationem harum voluptas? Amet maxime placeat
+          consequatur incidunt accusantium! Facere dignissimos fugit magni
+          asperiores minus natus nihil numquam enim?
+        </p>
       </div>
-      <div>
-        <Link to='/company/history'>History</Link>
-      </div>
-      <div>
-        <Link to='/contact'>Contact</Link>
-      </div>
-      <a href='/about'>Reg link</a>
-      <a href='https://www.gatsbyjs.com/docs/'>Gatsby docs</a>
     </Layout>
   );
 }
