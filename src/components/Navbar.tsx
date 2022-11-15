f9df90d4d22c82de1e79c14ce3931935d6bb0b37
Jarda Korinek <korinek14.j@gmail.com>
2022-11-15 21:01:27 +0100

    add new navbar component
    -added svg logo as a link
    -added react icon as a button
    -added all navbar links
    -added css for navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,45 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+import { FiAlignJustify } from 'react-icons/fi';
+
+import logo from '../assets/images/logo.svg';
+
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
-        </li>
-        <li>
-          <Link to='/about'>About</Link>
-        </li>
-        <li>
-          <Link to='/contact'>Contact</Link>
-        </li>
-        <li>
-          <Link to='/recipes'>Recipes</Link>
-        </li>
-        <li>
-          <Link to='/tags'>Tags</Link>
-        </li>
-      </ul>
+    <nav className='navbar'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <Link to='/'>
+            <img src={logo} alt='simple recipes' />
+          </Link>
+          <button type='button' className='nav-btn'>
+            <FiAlignJustify />
+          </button>
+        </div>
+
+        <div className='nav-links show-links'>
+          <Link to='/' className='nav-link' activeClassName='active-link'>
+            home
+          </Link>
+          <Link
+            to='/recipes'
+            className='nav-link'
+            activeClassName='active-link'
+          >
+            recipes
+          </Link>
+          <Link to='/tags' className='nav-link' activeClassName='active-link'>
+            tags
+          </Link>
+          <Link to='/about' className='nav-link' activeClassName='active-link'>
+            about
+          </Link>
+          <div className='nav-link contact-link'>
+            <Link to='/contact'>contact</Link>
+          </div>
+        </div>
+      </div>
     </nav>
   );
 }
--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,3 +1,5 @@
 declare module '*.module.scss';
 
 declare module 'styled-components';
+
+declare module '*.svg';
