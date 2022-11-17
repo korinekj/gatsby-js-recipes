61feea011788b6401c692eecfc2d65f4bae6af29
Jarda Korinek <korinek14.j@gmail.com>
2022-11-17 21:58:24 +0100

    SUCCESSFULLY MIGRATED TO GATSBY V5 AND INSTALLED GATSBY-PLUGIN-IMAGE BOI

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,10 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-sass`],
+  plugins: [
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+  ],
 };
