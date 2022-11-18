5e76a42ac4e2caaac5d2a25ceb919502fa948cc0
Jarda Korinek <korinek14.j@gmail.com>
2022-11-18 19:46:37 +0100

    GRAPHQL
    -updated gatsby config with siteMetadata for testing my first graphQL query
    -for testing graphQL queries, we have graphiQL in web browser

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,26 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'Simply Recipes',
+    description: 'Nice and clean recipes site',
+    author: 'John Doe',
+    person: {
+      name: 'jarda',
+      age: '28',
+    },
+    simpleData: ['item 1', 'item 2'],
+    complexData: [
+      {
+        name: 'jarda',
+        age: '28',
+      },
+      {
+        name: 'michal',
+        age: '25',
+      },
+    ],
+  },
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-image`,
