5123037f0bb812e7acaa7048c69b933818b76084
Jarda Korinek <korinek14.j@gmail.com>
2022-11-20 12:25:57 +0100

    complete test gallery component
    -useStaticQuery for quering images
    -map through all images and display them
    -added styles with css modules

--- src/examples/Gallery.tsx
+++ src/examples/Gallery.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { useStaticQuery, graphql } from 'gatsby';
 import { GatsbyImage } from 'gatsby-plugin-image';
 
+import * as styles from './gallery.module.scss';
+
 const query = graphql`
   query {
     allFile(filter: { extension: { ne: "svg" } }) {
@@ -13,6 +15,8 @@ const query = graphql`
             layout: FIXED
             placeholder: BLURRED
             transformOptions: { grayscale: true }
+            width: 200
+            height: 200
           )
         }
       }
@@ -23,10 +27,27 @@ const query = graphql`
 function Gallery() {
   const data = useStaticQuery(query);
 
-  console.log(data);
+  // const nodes = data.allFile.nodes;
+  const {
+    allFile: { nodes },
+  } = data;
+
   return (
-    <div>
-      <h2>simple image gallery</h2>
+    <div className={styles.wrap}>
+      {nodes.map((img: any, index: number) => {
+        const { name } = img;
+        return (
+          // eslint-disable-next-line react/no-array-index-key
+          <article key={index}>
+            <GatsbyImage
+              image={img.childImageSharp.gatsbyImageData}
+              alt={img.name}
+              className={styles.test}
+            />
+            <p className={styles.p}>{name}</p>
+          </article>
+        );
+      })}
     </div>
   );
 }
