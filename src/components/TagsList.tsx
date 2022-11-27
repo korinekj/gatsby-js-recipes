1cda050a18940cae8e5bd03d8c11eb645b24e129
Jarda Korinek <korinek14.j@gmail.com>
2022-11-27 20:21:49 +0100

    add tags page

--- src/components/TagsList.tsx
+++ src/components/TagsList.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import setupTags from '../utils/setupTags';
 
 interface Props {
@@ -9,14 +10,23 @@ function TagsList(props: Props) {
   const { recipes } = props;
 
   const newTags = setupTags(recipes);
+  console.log(newTags);
 
   return (
-    <div>
-      {newTags.map((tag, index) => {
-        console.log(tag);
-        // eslint-disable-next-line react/no-array-index-key
-        return <p key={index}>{tag}</p>;
-      })}
+    <div className='tag-container'>
+      <h4>recipes</h4>
+      <div className='tags-list'>
+        {newTags.map((tag, index) => {
+          console.log(tag);
+          return (
+            // eslint-disable-next-line react/no-array-index-key
+            <Link to={tag[0]} key={index}>
+              {`${tag[0]} (${tag[1]})`}
+              <br />
+            </Link>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -1,12 +1,53 @@
 import React from 'react';
+import { graphql, Link } from 'gatsby';
 import Layout from '../components/Layout';
+import setupTags from '../utils/setupTags';
 
-function Tags() {
+interface Props {
+  data: { allContentfulRecipe: { nodes: [] } };
+}
+
+function Tags(props: Props) {
+  const {
+    data: {
+      allContentfulRecipe: { nodes: tags },
+    },
+  } = props;
+
+  const newTags = setupTags(tags);
+
+  console.log(newTags);
+
   return (
     <Layout>
-      <h1>Tags page</h1>
+      <main className='page'>
+        <section className='tags-page'>
+          {newTags.map((tag, index) => {
+            const [text, value] = tag;
+            return (
+              // eslint-disable-next-line react/no-array-index-key
+              <Link to={text} key={index} className='tag'>
+                <h5>{text}</h5>
+                <p>{`${value} recipe`}</p>
+              </Link>
+            );
+          })}
+        </section>
+      </main>
     </Layout>
   );
 }
+
+export const query = graphql`
+  query {
+    allContentfulRecipe(sort: { title: ASC }) {
+      nodes {
+        content {
+          tags
+        }
+      }
+    }
+  }
+`;
 
 export default Tags;
