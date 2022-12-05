a4647f638d042e3629af45ab8f38b0e60b3c9bcb
Jarda Korinek <korinek14.j@gmail.com>
2022-12-05 23:58:50 +0100

    changed name in Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className='page-footer'>
       <p>
-        &copy; {new Date().getFullYear()} <span>SimpleRecipes</span> Built with{' '}
+        &copy; {new Date().getFullYear()} <span>SimplyRecipes</span> Built with{' '}
         <a href='https://www.gatsbyjs.com/'>Gatsby</a>
       </p>
     </footer>
