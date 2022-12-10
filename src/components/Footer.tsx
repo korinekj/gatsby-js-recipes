09101189ccf8bedf4a33fce49b07b3563fd448d9
Jarda Korinek <korinek14.j@gmail.com>
2022-12-10 19:51:58 +0100

    add my name to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,8 @@ function Footer() {
   return (
     <footer className='page-footer'>
       <p>
-        &copy; {new Date().getFullYear()} <span>SimplyRecipes</span> Built with{' '}
+        &copy; {new Date().getFullYear()} Jaroslav Kořínek,
+        <span>SimplyRecipes</span> Built with{' '}
         <a href='https://www.gatsbyjs.com/'>Gatsby</a>
       </p>
     </footer>
